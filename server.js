9599fc5437498cc001228a4cd0755609ce466642
tr0ncoduro <erick.parada101@gmail.com>
2024-06-25 13:49:36 -0600

    test for production

--- server.js
+++ server.js
@@ -28,8 +28,8 @@ app.use((err, req, res, next) => {
 
 app.post('/api/data', (req, res) => {
 	const data = req.body;
-	console.log('Received data', data);
-	res.json({ message: 'Data received successfully', receivedData: data });
+	console.log('Received data2', data);
+	res.json({ message: 'Data2 received successfully', receivedData: data });
 });
 
 
