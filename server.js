0c1f18a1f34d600a8e1b93a03639f67026d6ef84
tr0ncoduro <erick.parada101@gmail.com>
2024-06-25 13:29:48 -0600

    w

--- server.js
+++ server.js
@@ -1,15 +1,29 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app =  express();
 const port = process.env.PORT || 4000;
 
 //middlewares
-app.use(express.json());
+app.use(bodyParser.json());
+app.use(
+	bodyParser.urlencoded({
+		extended : true,
+	})
+);
 app.use(cors());
 
 app.get('/', (req, res) => {
 	res.json({'message': 'ok'});
+})
+
+app.use((err, req, res, next) => {
+	const statusCode = err.statusCode || 500;
+	console.error(err.message, err.stack);
+	res.status(statusCode).json({'message' : err.message});
+
+	return;
 })
 
 app.post('/api/data', (req, res) => {
